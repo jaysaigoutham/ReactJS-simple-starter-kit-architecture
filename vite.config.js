7fdbe03c2c49c600664e1c2dde281605eb2e3737
Jayasai Goutheman <gouthaman93@gmail.com>
2026-02-11 12:14:02 +0530

    Adding eslint, vitest & unit test

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js',
   }
 })
